00cb67592c70dc28aa198012b3148a5fb8badb63
twalt007 <tatumwalter@gmail.com>
2019-12-07 18:01:51 -0800

    added server routes for client, and built client for deploy

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const db = require('./db');
-const {buildUrl} = require('./helpers')
 const routes = require('./routes')
+const path = require('path');
+
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, 'client','dist')));
+
 app.use(routes);
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','dist','index.html'));
+})
 
 app.listen(PORT, ()=>{
     console.log('server is running at @ localhost:' + PORT);
