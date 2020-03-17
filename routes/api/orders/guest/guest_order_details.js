8caef07eb40d7c5ad65cee1445cd3b8419636227
twalt007 <tatumwalter@gmail.com>
2020-03-16 22:02:36 -0700

    sudo coded solution to response data

--- routes/api/orders/guest/guest_order_details.js
+++ routes/api/orders/guest/guest_order_details.js
@@ -3,32 +3,58 @@ const db = require('../../../../db');
 module.exports = async (req, res, next) => {
     
     try{
-        const {params} = req;
-        // const {order_id} = req.params;
-        const {query} = req;
-        //console.log("order_id: ", order_id, "email: ", email);
-        console.log("query: ", query);
-        // if(!order_id || isNaN(order_id)){
-        //     const error = new ApiError(422, "Invalid order id sent")
-        //     next(error);
-        //     return;
-        // }
-        // if(!email){
-        //     const error = new ApiError(400, "Invalid email sent")
-        //     next(error);
-        //     return;
-        // }
+        const {order_id} = req.params;
+        const {email} = req.query;
+
+        console.log("order_id: ", order_id, "email: ", email);
+
+        if(!order_id){
+            const error = new ApiError(422, "Invalid order id sent")
+            next(error);
+            return;
+        }
+        if(!email){
+            const error = new ApiError(400, "Invalid email sent")
+            next(error);
+            return;
+        }
         
-        // console.log('order_id: ', order_id);
+        const [[ordersQuery]] = await db.execute(`
+        SELECT email, cartId FROM orders WHERE pid=?
+        `, [order_id]);
+
+        console.log("email: ", ordersQuery.email);
+
+        // if (email === orderEmail){
+        //     const [[ results ]] = await db.query(`
+        //     SELECT `)
+        // }
+
+        // itemCount --> from cartItems "Select Sum(quantity) as itemCount where cardId = ?"
+        // total --> getCartTotals (cartId) 
+        // createdAt --> from orders
+        // id --> order id/Pid   from orders
+        // status --> fromo orders
+        // items --> [    reference add_items_to_cart
+        //     {
+        //         each: from products where productID taken from 
+        //         quantity: from ci where cartID
+        //         total:
+        //         id:
+        //         product:{
+        //             name:
+        //             id:
+        //             thumbnail: {
+        //                 altTect:
+        //                 url:
+        //             }
+        //         }
+        //     }
+        // ]
 
 
-        // const [orderEmail] = await db.execute(`
-        // SELECT email FROM orders WHERE pid=?
-        // `, [order_id]);
 
-        // console.log("email: ", orderEmail);
-
-        // res.send("I work");
+        res.send("I work");
 
     }catch(error){
         console.log("error", error);
