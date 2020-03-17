c8b39066849ac9c6e75cd266797d5e4aeb322320
twalt007 <tatumwalter@gmail.com>
2020-03-16 21:37:20 -0700

    resolved email-params issue - - > res.query"

--- routes/api/orders/guest/guest_order_details.js
+++ routes/api/orders/guest/guest_order_details.js
@@ -3,28 +3,33 @@ const db = require('../../../../db');
 module.exports = async (req, res, next) => {
     
     try{
-        const {order_id, email} = req.params;
-        console.log("order_id: ", order_id, "email: ", email);
+        const {params} = req;
+        // const {order_id} = req.params;
+        const {query} = req;
+        //console.log("order_id: ", order_id, "email: ", email);
+        console.log("query: ", query);
+        // if(!order_id || isNaN(order_id)){
+        //     const error = new ApiError(422, "Invalid order id sent")
+        //     next(error);
+        //     return;
+        // }
+        // if(!email){
+        //     const error = new ApiError(400, "Invalid email sent")
+        //     next(error);
+        //     return;
+        // }
         
-        if(!order_id || isNaN(order_id)){
-            const error = new ApiError(422, "Invalid order id sent")
-            next(error);
-            return;
-        }
-        if(!email){
-            const error = new ApiError(422, "Invalid email sent")
-            next(error);
-            return;
-        }
-        
-        console.log('order_id: ', order_id);
+        // console.log('order_id: ', order_id);
 
-        const [ orderEmail ] = await db.query(`
-        SELECT email FROM orders WHERE pid = ?
-        `, [order_id]);
-        console.log("email: ", orderEmail);
 
-        res.send("I work");
+        // const [orderEmail] = await db.execute(`
+        // SELECT email FROM orders WHERE pid=?
+        // `, [order_id]);
+
+        // console.log("email: ", orderEmail);
+
+        // res.send("I work");
+
     }catch(error){
         console.log("error", error);
         next(error); 
