812c0352d703c41584ba191a74958e7d5589913e
twalt007 <tatumwalter@gmail.com>
2020-03-22 09:06:51 -0700

    API query workinggit add .  need to fix client proccesssing --> 'name' undefined.

--- routes/api/orders/guest/guest_order_details.js
+++ routes/api/orders/guest/guest_order_details.js
@@ -1,13 +1,14 @@
 const db = require('../../../../db');
-const { getCartTotals } = require('../../../../helpers/index')
+const { buildUrl, getCartTotals } = require('../../../../helpers/index')
 
 module.exports = async (req, res, next) => {
     
     try{
         const {order_id} = req.params;
         const {email} = req.query;
+        let orderInfo = null;
 
-        console.log("order_id: ", order_id, "email: ", email);
+
 
         if(!order_id){
             const error = new ApiError(422, "Invalid order id sent")
@@ -20,14 +21,16 @@ module.exports = async (req, res, next) => {
             return;
         }
         
-        const [[ordersEmail]] = await db.execute(`
+
+
+        const [[ creds ]] = await db.execute(`
         SELECT email, cartId FROM orders WHERE pid=?
         `, [order_id]);
 
-        console.log("email: ", ordersEmail.email, "id: ", ordersEmail.cartId);
+        if (email === creds.email){
+            
+            const total = await getCartTotals(creds.cartId);
 
-        if (email === ordersEmail.email){
-            const total = await getCartTotals(ordersEmail.cartId);
             const [[ cart ]] = await db.query(`
             SELECT o.createdAt, o.pid AS id, os.name AS status 
             FROM orders AS o 
@@ -35,62 +38,38 @@ module.exports = async (req, res, next) => {
             WHERE o.pid=?`, [order_id]);
             
             const [cartData] = await db.execute(`
-            SELECT p.cost AS \`each\`, ci.quantity, (cost*quantity) AS total,  FROM cartItems AS ci 
+            SELECT p.cost AS \`each\`, ci.quantity, (cost*quantity) AS total, ci.pid AS id, p.name, p.pid AS productId, i.altText, i.type, i.file FROM cartItems AS ci 
             JOIN products AS p ON p.id = ci.productId
             JOIN images AS i ON i.productId = p.id
-            WHERE ci.id=?`, [ordersEmail.cartId]);
+            WHERE ci.cartId=? AND i.type="thumbnail"`, [creds.cartId]);
 
-            const output = {
+            const items = cartData.map( item => {
+                const {productId, altText, file, type, ...p} = item;
+                return {
+                    ...p,
+                    id: productId,
+                    thumbnail: {
+                        altText,
+                        url: buildUrl(req, type, file)
+                    }
+                }
+            })
+
+            console.log("items: ", items, "cardData: ", cartData);
+
+            orderInfo = {
                 itemCount: total.items,
                 total: total.cost,
                 ...cart,
-                cartData
+                items
             }
             
-            res.send(output);
         }
-        //     {
-        //         each: from products where productID taken from 
-        //         quantity: from ci where cartID
-        //         total:
-        //         id:
-        //         product:{
-        //             name:
-        //             id:
-        //             thumbnail: {
-        //                 altTect:
-        //                 url:
-        //             }
-        //         }
-        //     }
-        // ]
-
-
-
-        
+        res.send(orderInfo);
 
     }catch(error){
         console.log("error", error);
         next(error); 
     }
     
-    
-    //recieve data
-    //check that have info
-    //check that email aligns with the order id nummber
-    //return tuff
 }
-
-
-
- // const [[ results ]] = await db.query(`
-            // SELECT ci.productID, ci.quantity, o.createdAt, p.description, p.cost, i.file
-            // FROM orders AS o 
-            // JOIN cartItems AS ci
-            // ON ci.cartId = o.cartID
-            // JOIN products AS p
-            // ON p.id = ci.productId
-            // JOIN images AS i
-            // ON i.productId = p.id
-            // WHERE o.pid=?`, [order_id]);
-            // res.send(results);
