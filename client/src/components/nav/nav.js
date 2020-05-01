0086d82e522a649b073d97bc542ae6f0c5dec835
twalt007 <tatumwalter@gmail.com>
2020-04-30 21:16:57 -0700

    groundwork for mobile responsiveness - got header image to resize, text font size to work ok, media queries basically constructed, working on getting boostrap grid set-up better

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -5,23 +5,50 @@ import CartWidget from "./cart_widget"
 
 export default props => {
     return (
-        <ul className="main-nav">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/products">Products</Link>
-            </li>
-            <li>
-                <Link to="/about">About Us</Link>
-            </li>
-            <li>
-                <Link to="/services">Services</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-            <CartWidget/>
-        </ul>
+        <div className="nav">
+            <div className="dropdown">
+                <i className="material-icons">menu</i>
+                <div className="dropdown-cntnt">
+                    <ul className="main-nav">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/services">Services</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <CartWidget/>
+            </div>
+            <div className="standard-menu">
+                <ul className="main-nav">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/services">Services</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <CartWidget/>
+                </ul>
+            </div>
+        </div>
     );
 }
